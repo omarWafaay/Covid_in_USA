e62ea48f7b56bd01eb0ee60f5628fe73f754359a
Omar Wafaay <omarheshamwafaie@gmail.com>
2022-09-25 22:46:02 +0200

    re-run the environment

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # importing required libraries
-import pandas as pd
 import os
 from pathlib import Path
-import dash
 
-from dash import html, dcc, Input, Output
+import dash
+import pandas as pd
 import plotly.express as px
+from dash import html, dcc, Input, Output
 
 # Colleting the data
 data_file = os.path.join(Path(__file__).parent, 'data', 'time_series_covid19_confirmed_US.csv')
@@ -20,9 +20,8 @@ df_melt = df_state_lvl.melt(id_vars=['Province_State'],
 # Selecting options in province states
 ops = df_melt['Province_State'].unique()
 
-
 app = dash.Dash()
-server=app.server #configure the server
+server = app.server  # configure the server
 app.layout = html.Div(children=
                       [html.Div('Hello World From Dash.'),
                        html.H1('H1 tag here'),
@@ -36,11 +35,15 @@ app.layout = html.Div(children=
                        ]
 
                       )
+
+
 @app.callback(Output(component_id='fig1', component_property='figure'),
-              Input(component_id='dropdown',component_property= 'value'))
+              Input(component_id='dropdown', component_property='value'))
 def update_graph(state):
     df_state = df_melt.loc[df_melt['Province_State'] == state]
-    fig = px.line(df_state, x = 'variable', y ='value', title = f'{state} cumulative case counts')
+    fig = px.line(df_state, x='variable', y='value', title=f'{state} cumulative case counts')
     return fig
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
